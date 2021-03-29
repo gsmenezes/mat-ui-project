bc8c50fecf6c7849853c00533bb4549fcb420909
gimenezes <menezes.s.gilda@gmail.com>
2021-03-29 15:10:58 -0300

    add typography and button to create note page

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from "react";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import Container from "@material-ui/core/Container";
 
 export default function Create() {
   return (
-    <div>
-      Create page
-    </div>
-  )
+    <Container>
+      <Typography
+        variant="h5"
+        component="h2"
+        color="textSecondary"
+        gutterBottom
+      >
+        Create a new note Note
+      </Typography>
+      <Button
+        type="submit"
+        color="primary"
+        variant="outlined"
+        onClick={() => console.log("Hello! You clicked me..")}
+      >
+        Submit
+      </Button>
+    </Container>
+  );
 }
