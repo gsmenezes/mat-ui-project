df38deea179c8991381b9b9d1ad9364ac14c5c0e
gimenezes <menezes.s.gilda@gmail.com>
2021-03-29 18:00:34 -0300

    add forms

--- src/App.js
+++ src/App.js
@@ -1,19 +1,40 @@
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import Notes from './pages/Notes'
-import Create from './pages/Create'
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Notes from "./pages/Notes";
+import Create from "./pages/Create";
+import { createMuiTheme, ThemeProvider } from "@material-ui/core";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#006D77",
+    },
+    secondary: {
+      main: "#FFDDD2",
+    },
+  },
+  typography: {
+    fontFamily: "Quicksand",
+    fontWeightLight: 400,
+    fontWeightRegular: 500,
+    fontWeightMedium: 600,
+    fontWeightBold: 700,
+  },
+});
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/">
-          <Notes />
-        </Route>
-        <Route path="/create">
-          <Create />
-        </Route>
-      </Switch>
-    </Router>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Notes />
+          </Route>
+          <Route path="/create">
+            <Create />
+          </Route>
+        </Switch>
+      </Router>
+    </ThemeProvider>
   );
 }
 
--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,10 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
 import Container from "@material-ui/core/Container";
-import SendIcon  from "@material-ui/icons/Send";
+import SendIcon from "@material-ui/icons/Send";
+import { FormControlLabel, makeStyles } from "@material-ui/core";
+import TextField from "@material-ui/core/TextField";
+import FormControl from "@material-ui/core/FormControl";
+import FormLabel from "@material-ui/core/FormLabel";
+import RadioGroup from "@material-ui/core/RadioGroup";
+import Radio from "@material-ui/core/Radio";
+
+const useStyles = makeStyles({
+  field: {
+    marginTop: 20,
+    marginBottom: 20,
+    display: "block",
+  },
+});
 
 export default function Create() {
+  const classes = useStyles();
+
+  const [title, setTitle] = useState("");
+  const [details, setDetails] = useState("");
+  const [titleError, setTitleError] = useState(false);
+  const [detailsError, setDetailsError] = useState(false);
+  const [category, setCategory] = useState("todos");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setTitleError(false);
+    setDetailsError(false);
+
+    if (title === "") {
+      setTitleError(true);
+    }
+
+    if (details === "") {
+      setDetailsError(true);
+    }
+
+    if (title && details) {
+      console.log(title, details, category);
+    }
+  };
+
   return (
     <Container>
       <Typography
@@ -15,15 +55,54 @@ export default function Create() {
       >
         Create a new note Note
       </Typography>
-      <Button
-        type="submit"
-        color="primary"
-        variant="outlined"
-        onClick={() => console.log("Hello! You clicked me..")}
-        startIcon={<SendIcon />}
-      >
-        Submit
-      </Button>
+
+      <form noValidate autoComplete="off" onSubmit={handleSubmit}>
+        <TextField
+          onChange={(e) => setTitle(e.target.value)}
+          className={classes.field}
+          label="Note title"
+          variant="outlined"
+          fullWidth
+          required
+          error={titleError}
+        />
+        <TextField
+          onChange={(e) => setDetails(e.target.value)}
+          className={classes.field}
+          label="Details"
+          variant="outlined"
+          fullWidth
+          multiline
+          rows={4}
+          required
+          error={detailsError}
+        />
+
+        <FormControl className={classes.field}>
+          <FormLabel>Note Category</FormLabel>
+          <RadioGroup
+            value={category}
+            onChange={(e) => setCategory(e.target.value)}
+          >
+            <FormControlLabel value="todos" control={<Radio />} label="To Do" />
+            <FormControlLabel
+              value="reminders"
+              control={<Radio />}
+              label="Reminder"
+            />
+            <FormControlLabel value="work" control={<Radio />} label="Work" />
+          </RadioGroup>
+        </FormControl>
+
+        <Button
+          type="submit"
+          color="primary"
+          variant="outlined"
+          startIcon={<SendIcon />}
+        >
+          Submit
+        </Button>
+      </form>
     </Container>
   );
 }
