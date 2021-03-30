de665ff81b9f4f53ea69c2bc1f679c71eb30edba
gimenezes <menezes.s.gilda@gmail.com>
2021-03-29 21:45:03 -0300

    add delete and menu button, change layout

--- src/Components/Layout/index.js
+++ src/Components/Layout/index.js
@@ -1,18 +1,88 @@
 import React from "react";
-import {makeStyles} from '@material-ui/core';
+import { makeStyles } from "@material-ui/core";
+import Drawer from "@material-ui/core/Drawer";
+import Typography from "@material-ui/core/Typography";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import { SubjectOutlined, AddCircleOutlineOutlined } from "@material-ui/icons";
+import { useHistory, useLocation } from "react-router";
 
-const useStyles = makeStyles({
-  page: {
-    background: "#EDF6F9",
-    width: "100%",
-    height: '100vh'
-  },
+const drawerWidth = 240;
+
+const useStyles = makeStyles((theme) => {
+  return {
+    page: {
+      background: "#EDF6F9",
+      width: "100%",
+      height: "100vh",
+      padding: theme.spacing(3),
+    },
+    drawer: {
+      width: drawerWidth,
+    },
+    drawerPaper: {
+      width: drawerWidth,
+    },
+    root: {
+      display: "flex",
+    },
+    active: {
+      background: "#E29578",
+    },
+    title: {
+      padding: theme.spacing(2)
+    }
+  };
 });
 
 const Layout = ({ children }) => {
   const classes = useStyles();
+  const history = useHistory();
+  const location = useLocation();
+
+  const menuItems = [
+    {
+      text: "My Notes",
+      icon: <SubjectOutlined color="secondary" />,
+      path: "/",
+    },
+    {
+      text: "Create Note",
+      icon: <AddCircleOutlineOutlined color="secondary" />,
+      path: "/create",
+    },
+  ];
+
   return (
-    <div>
+    <div className={classes.root}>
+      <Drawer
+        className={classes.drawer}
+        variant="permanent"
+        anchor="left"
+        classes={{ paper: classes.drawerPaper }}
+      >
+        <div>
+          <Typography variant="h5" className={classes.title}>Gi Notes</Typography>
+        </div>
+
+        <List>
+          {menuItems.map((item) => (
+            <ListItem
+              button
+              key={item.text}
+              onClick={() => history.push(item.path)}
+              className={
+                location.pathname === item.path ? classes.active : null
+              }
+            >
+              <ListItemIcon>{item.icon}</ListItemIcon>
+              <ListItemText primary={item.text} />
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
       <div className={classes.page}>{children}</div>
     </div>
   );
